a4488fa5318ca27d3276e6ba9674bb6c8a2bcba6
BaltazarGalvan <b_galvan@yahoo.com>
2023-04-14 12:27:51 -0600

    modulos commit

---
+++ imgArray.js
@@ -0,0 +1,60 @@
+const imgArray = [
+  {
+    class: "carousel-item active",
+    source: "./images/Banner.jpg",
+    altText: "Under Construction",
+    buttonClass: "active",
+    buttonAriaCurrent: 'aria-current="true"',
+  },
+  {
+    class: "carousel-item",
+    source: "./images/U-StuffBBlackSmith.png",
+    altText: "U-StuffBlackSmith",
+    buttonClass: "",
+    buttonAriaCurrent: "",
+  },
+  {
+    class: "carousel-item",
+    source: "./images/U-StuffWCarpenter.png",
+    altText: "StuffWCarpenter",
+    buttonClass: "",
+    buttonAriaCurrent: "",
+  },
+  {
+    class: "carousel-item",
+    source: "./images/U-StuffBCook.png",
+    altText: "U-StuffBCook",
+    buttonClass: "",
+    buttonAriaCurrent: "",
+  },
+  {
+    class: "carousel-item",
+    source: "./images/U-StuffWGarden.png",
+    altText: "U-StuffWGarden",
+    buttonClass: "",
+    buttonAriaCurrent: "",
+  },
+  {
+    class: "carousel-item",
+    source: "./images/U-StuffBHome.png",
+    altText: "U-StuffBHome",
+    buttonClass: "",
+    buttonAriaCurrent: "",
+  },
+  {
+    class: "carousel-item",
+    source: "./images/U-StuffWPets.png",
+    altText: "U-StuffWPets",
+    buttonClass: "",
+    buttonAriaCurrent: "",
+  },
+  {
+    class: "carousel-item",
+    source: "./images/U-StuffBTech.png",
+    altText: "U-StuffBTech",
+    buttonClass: "",
+    buttonAriaCurrent: "",
+  },
+];
+
+export default imgArray;
--- script.js
+++ script.js
@@ -1,67 +1,10 @@
 const carouselIndicators = document.querySelector(".carousel-indicators");
 const carouselInner = document.querySelector(".carousel-inner");
 
+import imgArray from "./imgArray.js";
+
 let carouselInnerHTML = "";
 let carouselIndicatorsHTML = "";
-
-const imgArray = [
-  {
-    class: "carousel-item active",
-    source: "./images/Banner.jpg",
-    altText: "Under Construction",
-    buttonClass: "active",
-    buttonAriaCurrent: 'aria-current="true"',
-  },
-  {
-    class: "carousel-item",
-    source: "./images/U-StuffBBlackSmith.png",
-    altText: "U-StuffBlackSmith",
-    buttonClass: "",
-    buttonAriaCurrent: "",
-  },
-  {
-    class: "carousel-item",
-    source: "./images/U-StuffWCarpenter.png",
-    altText: "StuffWCarpenter",
-    buttonClass: "",
-    buttonAriaCurrent: "",
-  },
-  {
-    class: "carousel-item",
-    source: "./images/U-StuffBCook.png",
-    altText: "U-StuffBCook",
-    buttonClass: "",
-    buttonAriaCurrent: "",
-  },
-  {
-    class: "carousel-item",
-    source: "./images/U-StuffWGarden.png",
-    altText: "U-StuffWGarden",
-    buttonClass: "",
-    buttonAriaCurrent: "",
-  },
-  {
-    class: "carousel-item",
-    source: "./images/U-StuffBHome.png",
-    altText: "U-StuffBHome",
-    buttonClass: "",
-    buttonAriaCurrent: "",
-  },
-  {
-    class: "carousel-item",
-    source: "./images/U-StuffWPets.png",
-    altText: "U-StuffWPets",
-    buttonClass: "",
-    buttonAriaCurrent: "",
-  },
-  {
-    class: "carousel-item",
-    source: "./images/U-StuffBTech.png",
-    altText: "U-StuffBTech",
-    buttonClass: "",
-    buttonAriaCurrent: "",
-  },
-];
 
 imgArray.forEach((carouselImg, index) => {
   carouselIndicatorsHTML =

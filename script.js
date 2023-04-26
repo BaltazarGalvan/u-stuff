9937ecc9fd3805d4663b8d94ca15345f14114707
BaltazarGalvan <b_galvan@yahoo.com>
2023-04-25 19:51:04 -0600

    production version commit

--- script.js
+++ script.js
@@ -6,26 +6,34 @@ import imgArray from "./imgArray.js";
 let carouselInnerHTML = "";
 let carouselIndicatorsHTML = "";
 
+document.querySelector("#navbarSideCollapse").addEventListener("click", () => {
+  document.querySelector(".offcanvas-collapse").classList.toggle("open");
+});
+
 imgArray.forEach((carouselImg, index) => {
+  console.log(carouselImg.nombre);
   carouselIndicatorsHTML =
     carouselIndicatorsHTML +
     `<button
-          style="background-color:purple"
+          style="background-color:white"
           type="button"
           data-bs-target="#main-carousel"
           data-bs-slide-to="${index}"
           aria-label="Slide ${index + 1}"
           class="${carouselImg.buttonClass}"
           ${carouselImg.buttonAriaCurrent}
-        ></button>`;
+        ></button>  `;
 
   carouselInnerHTML =
     carouselInnerHTML +
     `<div class="${carouselImg.class}">
           <img src="${carouselImg.source}" alt="${carouselImg.altText}" />
+          <div class="carousel-caption text-start">
+            <h4>${carouselImg.name}</h4>
+            <p>${carouselImg.description}</p>
+          </div>
         </div>`;
 });
 
 carouselIndicators.innerHTML = carouselIndicatorsHTML;
-// console.log(carouselIndicatorsHTML);
 carouselInner.innerHTML = carouselInnerHTML;
